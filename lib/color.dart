772852ec179591dd309e5473e6d694d4b5813c0a
kuosuuyu123 <409630299@gms.tku.edu.tw>
2022-12-23 03:06:21 +0800

    Initial commit

---
+++ lib/color.dart
@@ -0,0 +1,397 @@
+import 'package:flutter/material.dart';
+
+//注意使用這裡的素材更改樣式時，不能使用Hot reload，要使用Run，如此才會更新color.dart。
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//--------------------------------------------Homepage-----------------------------------------------//
+
+
+//background design---
+List<BoxDecoration> backgroundsH = [
+  BoxDecoration(
+    gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.orange.shade200,
+          Colors.orange.shade300,
+        ]
+    ),
+  ),//-----tech
+  BoxDecoration(
+    gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.black,
+          Colors.purple,
+        ]
+    ),
+  ),
+
+
+];
+
+//appbar design---
+List<Color> Happbar=[
+  Color.fromARGB(255, 93, 59, 38),
+  Color.fromARGB(255, 66, 33, 31),
+];
+
+//appbar Text design---
+List<TextStyle> HbarText=[
+  TextStyle(fontSize: 30,color:Color.fromARGB(173, 255, 125, 0),),
+  TextStyle(fontSize: 30,color: Color.fromARGB(255, 147, 44, 36),) ,
+];
+
+//-----------------Card-----------------
+
+//Card design---
+List<BoxDecoration> HCardground=[
+  BoxDecoration(
+    gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.orange.shade200,
+          Colors.orange.shade300,
+        ]
+    ),
+  ),//-----tech
+  BoxDecoration(
+    gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.red.shade300,
+          Colors.white,
+        ]
+    ),
+  ),
+];
+
+//'Asset' Text-------
+List<TextStyle> HCardAsText=[
+  TextStyle(color: Colors.orange.shade300,fontSize: 30),
+  TextStyle(color: Colors.purple.shade300,fontSize: 30),
+];
+
+//'Asset' (money)-------
+List<TextStyle> HCardMAsText=[
+  TextStyle(color: Colors.orange.shade300,fontSize: 30),
+  TextStyle(color: Colors.purple.shade300,fontSize: 30),
+];
+
+//'income' Text-------
+List<TextStyle> HCardInText=[
+  TextStyle(color: Colors.orange.shade300,fontSize: 20),
+  TextStyle(color: Colors.purple.shade300,fontSize: 20),
+];
+
+//$income (money)------
+List<TextStyle> HCardMInText=[
+  TextStyle(color: Colors.orange.shade300,fontSize: 20,fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.purple.shade300,fontSize: 20,fontWeight: FontWeight.bold),
+];
+
+//'Expense' Text------
+List<TextStyle> HCardExText=[
+  TextStyle(color: Colors.orange.shade300,fontSize: 20),
+  TextStyle(color: Colors.purple.shade300,fontSize: 20),
+];
+
+//$Expense (money)------
+List<TextStyle> HCardMExText=[
+  TextStyle(color: Colors.orange.shade300,fontSize: 20,fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.purple.shade300,fontSize: 20,fontWeight: FontWeight.bold),
+];
+
+
+//-----------------list-----------------------
+
+
+//list Container------
+List<BoxDecoration> listContainer=[
+  BoxDecoration(
+
+    borderRadius: BorderRadius.circular(10),
+    color: Color.fromARGB(116, 243, 243, 243),
+    border: Border.all(width: 2,color: Color.fromARGB(229, 253, 253, 253)),
+
+  ),
+  BoxDecoration(
+
+    borderRadius: BorderRadius.circular(10),
+    color: Color.fromARGB(116, 243, 243, 243),
+    border: Border.all(width: 2,color: Color.fromARGB(229, 253, 253, 253)),
+
+  ),
+
+];
+
+//list title Text-------
+List<TextStyle> listtitle=[
+  TextStyle(color: Colors.brown.shade800),
+  TextStyle(color: Colors.purple.shade800),
+];
+
+//list trailing Text-----
+List<TextStyle> listtrailing=[
+  TextStyle(color: Colors.brown.shade800),
+  TextStyle(color: Colors.purple.shade800),
+];
+
+//Divider-------
+List<Divider> listdivider=[
+  Divider(color:Color.fromARGB(26, 0, 0, 0),thickness: 2,),
+  Divider(color:Color.fromARGB(26, 0, 0, 0),thickness: 2,),
+];
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//-----------------------------Month Page------------------------------
+
+//background design---
+List<BoxDecoration> backgrounds = [
+  BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [Color.fromARGB(174, 227, 180, 184),
+              Color.fromARGB(174, 222, 28, 49),],
+    ),
+  ),//-----tech
+  BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [Color.fromARGB(173, 200, 141, 133),
+                Color.fromARGB(255, 117, 131, 145),
+                Color.fromARGB(173, 23, 32, 51),],
+      ),
+  ),
+  //----red
+  BoxDecoration(
+    gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromARGB(255, 40, 143, 113),
+          Color.fromARGB(255, 180, 160, 128),
+          Color.fromARGB(255, 180, 160, 128),
+          Color.fromARGB(255, 194, 124, 93),
+          Color.fromARGB(255, 170, 97, 74),
+        ]
+    ),
+  ),
+];
+
+
+//-----------------------------------彈跳視窗--------------
+//AlertDiog design---
+List<Color> stylealert=[
+  Colors.red.shade200,
+  Color.fromARGB(255, 154, 122, 131),
+  Color.fromARGB(255, 194, 146, 98),
+];
+
+//AlerDiog Text design---
+//125、129行==>labelstyle:Textstyle(),
+List<TextStyle> styleALText=[
+  TextStyle(color: Colors.purple[300],fontWeight: FontWeight.bold),
+  TextStyle(color: Color.fromARGB(138,44,30,30), fontWeight: FontWeight.bold),
+  TextStyle(color: Color.fromARGB(138,44,30,30),fontWeight: FontWeight.bold),
+];
+
+//Switch design---
+List<Color> styleswitch=[
+  Colors.purple.shade300,
+  Color.fromARGB(138, 44, 30, 30),
+  Color.fromARGB(138, 44, 30, 30),
+];
+
+//ElevatedButton design---
+List<ButtonStyle> bstyle=[
+  ElevatedButton.styleFrom(backgroundColor: Colors.teal[200],),
+  ElevatedButton.styleFrom(backgroundColor:Color.fromARGB(100, 167, 105, 189),),
+  ElevatedButton.styleFrom(backgroundColor:Color.fromARGB(204, 64, 112, 66),),
+];
+
+//ElevatedButton Text design---
+List<TextStyle> styleETEXT=[
+  TextStyle(color: Colors.purple[300],fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.orange[100],fontWeight: FontWeight.bold),
+];
+
+
+//--------------------------------appbar---------------------
+//appbar design---
+List<Color> styleappbar=[
+  Colors.pink.shade300,
+  Color.fromARGB(128, 53, 53, 107),
+  Colors.teal.shade600,
+];
+
+//appbar Text design---
+List<TextStyle> stylebarText=[
+  TextStyle(fontSize: 30,color: Colors.pink[100]),
+  TextStyle(fontSize: 30,color: Colors.purple[100]),
+  TextStyle(fontSize: 30,color: Colors.teal[100]),
+
+];
+
+
+
+//--------------------------------Card------------------------
+//Card design---
+List<BoxDecoration> Cardground=[
+  BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+      color: Color.fromARGB(138, 131, 91, 91),
+      boxShadow: [
+        BoxShadow(
+            color: Colors.grey.shade500,
+            offset: Offset(4.0, 4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0),
+        BoxShadow(
+            color: Colors.red,
+            offset: Offset(-4.0, -4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0),
+      ]),
+  BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+      color:  Color.fromARGB(128, 50, 10, 23),
+      boxShadow: [
+        BoxShadow(
+            color: Colors.grey.shade500,
+            offset: Offset(4.0, 4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0),
+        BoxShadow(
+            color: Colors.teal.shade200,
+            offset: Offset(-4.0, -4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0),
+      ]),
+
+  BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+      color:Color.fromARGB(173, 13, 72, 72),
+      boxShadow: [
+        BoxShadow(
+            color: Colors.grey.shade500,
+            offset: Offset(4.0, 4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0),
+        BoxShadow(
+            color: Colors.teal,
+            offset: Offset(-4.0, -4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0),
+      ]),//Black Card-------
+
+];
+
+//'Asset' Text
+List<TextStyle> CardAsText=[
+  TextStyle(color: Colors.white,fontSize: 30),
+  TextStyle(color: Colors.orange[100],fontSize: 30),
+  TextStyle(color: Colors.white,fontSize: 30),
+];
+
+//'Asset' (Money)
+List<TextStyle> CardMAsText=[
+  TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.orange[100],fontSize: 30,fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),
+];
+
+//Circle in Card----
+List<Color> styleCirle=[
+  Colors.grey.shade100,
+  Colors.orange.shade100,
+  Colors.grey.shade200,
+];
+
+//up arrow design---
+List<Color> styleup=[
+  Colors.teal,
+  Colors.green.shade900,
+  Colors.green.shade900,
+];
+
+//'income' Text-------
+List<TextStyle> CardInText=[
+  TextStyle(color: Colors.pink[100],),
+  TextStyle(color: Colors.grey[400],),
+  TextStyle(color: Colors.grey[400])
+];
+
+//$income (money)------
+List<TextStyle> CardMInText=[
+  TextStyle(color: Colors.pink[100],fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.grey[400],fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.grey[400],fontWeight: FontWeight.bold),
+];
+
+//down arrow design---
+List<Color> styledown=[
+  Colors.deepOrange.shade900,
+  Colors.red.shade900,
+  Colors.red.shade900,
+];
+
+//'Expense' Text------
+List<TextStyle> CardExText=[
+  TextStyle(color: Colors.pink[100],),
+  TextStyle(color: Colors.grey[400],),
+  TextStyle(color: Colors.grey[400])
+];
+
+//$Expense (money)------
+List<TextStyle> CardMExText=[
+  TextStyle(color: Colors.pink[100],fontWeight: FontWeight.bold),
+  TextStyle(color:Colors.grey[400],fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.grey[400],fontWeight: FontWeight.bold),
+];
+
+
+//-------------------------------List-------------------------
+//list Name amd Amount Text design---
+List<TextStyle> listname=[
+  TextStyle(color: Colors.white),
+  TextStyle(color: Colors.purple[900],),
+  TextStyle(color: Colors.purple[900],),
+];
+
+//list Date design------
+List<TextStyle> listDate=[
+  TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.purple[900],fontWeight: FontWeight.bold),
+  TextStyle(color: Colors.purple[900],fontWeight: FontWeight.bold),
+];
+
+
+//-----------------------------------FloatButton----------------
+//FloatButton design-----
+List<Color> styleFloatbutton=[
+  Colors.redAccent.shade400,
+  Color.fromARGB(173, 44, 70, 98),
+  Color.fromARGB(255, 124, 79, 79),
+];
+
+//FloatButton icon design---
+List<Icon> styleFloaticon=[
+  Icon(Icons.filter_vintage_outlined,color: Colors.red.shade100,),
+  Icon(Icons.dark_mode,color: Colors.yellowAccent,size: 30,),
+  Icon(Icons.park_rounded,color: Colors.green,size: 35,),
+];
+
+List<Divider> styledivider=[
+  Divider(color: Color.fromARGB(64, 222, 28, 49),thickness: 2,),
+  Divider(color: Color.fromARGB(77, 23, 32, 51),thickness: 2,),
+  Divider(color: Color.fromARGB(77, 170, 97, 74),thickness: 2,),
+];
